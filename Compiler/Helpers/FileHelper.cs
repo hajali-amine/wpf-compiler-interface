e64a7f2a94063ad5642200773dabe1bb093fd42e
unknown <aminehajali@insat.u-carthage.tn>
2022-03-28 13:46:13 +0200

    initial

---
+++ Compiler/Helpers/FileHelper.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Compiler.Helpers
+{
+    public static class FileHelper
+    {
+        public static async Task WriteFile(string text, string FileName)
+        {
+            await File.WriteAllTextAsync(FileName, text);
+        }
+
+        public static List<string> RunExe()
+        {
+            var proc = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = "a.exe",
+                    Arguments = "CeciEstUnTest.txt",
+                    UseShellExecute = false,
+                    RedirectStandardOutput = true,
+                    CreateNoWindow = true
+                }
+            };
+
+            List<string> result = new List<string>();
+
+            proc.Start();
+            while (!proc.StandardOutput.EndOfStream)
+            {
+                result.Add(proc.StandardOutput.ReadLine());
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Compiler/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using Compiler.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Compiler
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void OnCompileButtonClicked(object sender, RoutedEventArgs e)
+        {
+            _ = FileHelper.WriteFile(this.InputBox.Text, "CeciEstUnTest.txt");
+            string output = String.Join("\n", FileHelper.RunExe());
+            this.OutputBlock.Text = output;
+        }
+    }
+}
