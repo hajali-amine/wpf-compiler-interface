b5417d16d05d4c64bb726261bfb33e828ef08148
hajali-amine <amine.hajali@insat.ucar.tn>
2022-06-11 19:32:34 +0100

    update

--- MiniJavaCompiler/lex.bat
+++
@@ -1,3 +0,0 @@
-flex LexicalAnalyser.l
-bison -d SyntaxicAnalyser.y
-gcc -o parser.exe SyntaxicAnalyser.tab.c lex.yy.c
--- MiniJavaCompiler/parser_maker.bat
+++ MiniJavaCompiler/parser_maker.bat
@@ -1,3 +1,3 @@
 flex LexicalAnalyser.l
 bison -d SyntaxicAnalyser.y
-gcc -o parser.exe SyntaxicAnalyser.tab.c lex.yy.c
+gcc -o minijava_compiler.exe SyntaxicAnalyser.tab.c lex.yy.c semantic.c generator.c
